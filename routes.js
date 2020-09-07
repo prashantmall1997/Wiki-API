3f66c01273956b2f81b9e1e59c6b4471194441b0
Prashant Mall <prashantmall1997@gmail.com>
2020-09-07 12:01:32 +0530

    RESTFul API - Complete

--- routes.js
+++ routes.js
@@ -40,4 +40,71 @@ module.exports = function(app) {
         }
       });
     });
+
+  app.route("/articles/:articleTitle")
+    .get((req, res) => {
+      articlesModel.findOne({
+        title: req.params.articleTitle
+      }, (err, foundArticle) => {
+        if (!err) {
+          if (foundArticle) {
+            res.send(foundArticle);
+          } else {
+            res.send(`No article found with title: ${req.params.articleTitle}`);
+          }
+        } else {
+          res.send(err);
+        }
+      });
+    })
+
+    .put((req, res) => {
+      articlesModel.replaceOne({
+        title: req.params.articleTitle
+      }, {
+        title: req.body.title,
+        content: req.body.content
+      }, (err, result) => {
+        if (!err) {
+          if (result.n === 1) {
+            res.send("Successfully updated article.");
+          } else {
+            res.send(`No article found with title: ${req.params.articleTitle}`);
+          }
+        } else {
+          res.send(err);
+        }
+      });
+    })
+
+    .patch((req, res) => {
+      articlesModel.updateOne({
+        title: req.params.articleTitle
+      }, {
+        $set: req.body
+      }, (err, result) => {
+        if (!err) {
+          if (result.n === 1) {
+            res.send("Successfully updated article.");
+          } else {
+            res.send(`No article found with title: ${req.params.articleTitle}`);
+          }
+        } else {
+          res.send(err);
+        }
+      });
+    })
+
+    .delete((req, res) => {
+      articlesModel.deleteOne({
+        title: req.params.articleTitle
+      }, (err) => {
+        if (!err) {
+          res.send("Article deleted.");
+        } else {
+          res.send(err);
+        }
+      });
+    });
+
 };
