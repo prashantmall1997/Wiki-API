51452ae6c3eb5d97f3dda5f391879f0d3613ec7e
prashantmal1997 <prashantmall1997@gmail.com>
2021-05-28 18:03:23 -0400

    Modify for hosting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:8
 
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
@@ -7,25 +8,30 @@ const express = require("express");
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
 //Port
 const port = process.env.PORT || 3000;
 
 //Connect DB
-mongoose.connect('mongodb://localhost:27017/wikiDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.tzenv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 //Routes
-require('./routes')(app);
+require("./routes")(app);
 
 //Run Server
-app.listen(port, function() {
+app.listen(port, function () {
   console.clear();
   console.log(`Server started on port ${port}`);
 });
